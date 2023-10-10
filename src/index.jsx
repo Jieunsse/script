ac6853ff83f1013255f4e85ad5635c1c3181007f
jieunsse <jieunsse@gmail.com>
2023-10-10 12:33:48 +0900

    Error Page Routed.

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/Root';
+import Error from './Page/Error/Error';
+
 
 const router = createBrowserRouter([
   {
     path:'/',
     element: <Root />,
+    errorElement: <Error/>
   },
 ]);
 
