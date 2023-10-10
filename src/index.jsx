66a6b85cdaa63e33fe4cd491233be6e1e917f855
jieunsse <jieunsse@gmail.com>
2023-10-10 12:29:11 +0900

    Main page set up completed.

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/Root';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Root />,
+  },
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
---
+++ src/routes/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from '../Components/common/Modal/Modal';
+
+export default function Root() {
+    return (
+        <>
+            <Modal/>
+        </>
+    );
+}
+
+
